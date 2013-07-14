711ed8008b1a400e0113cb5f2a857385663408d7
Matthias Fischer <matthias@dotnetautor.de>
2013-07-14 14:54:22 +0200

    FeedReader hinzugefügt
    Beispielprojekt FeedReder hinzugefügt

---
+++ Buch.Beispiele/Kapitel 02/FeedReader/FeedReader/Code/Utilities/HttpExtentions.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FeedReader.Code.Utilities {
+  public static class HttpExtensions {
+    public static Task<HttpWebResponse> GetResponseAsync(this HttpWebRequest request) {
+      var taskComplete = new TaskCompletionSource<HttpWebResponse>();
+      request.BeginGetResponse(asyncResponse => {
+        try {
+          var responseRequest = (HttpWebRequest)asyncResponse.AsyncState;
+          var someResponse = (HttpWebResponse)responseRequest.EndGetResponse(asyncResponse);
+          taskComplete.TrySetResult(someResponse);
+        } catch (WebException webExc) {
+          var failedResponse = (HttpWebResponse)webExc.Response;
+          taskComplete.TrySetResult(failedResponse);
+        }
+      }, request);
+      return taskComplete.Task;
+    }
+
+    public static Task<Stream> GetRequestStreamAsync(this HttpWebRequest request) {
+      var taskComplete = new TaskCompletionSource<Stream>();
+      request.BeginGetRequestStream(asyncResponse => {
+        try {
+          var responseRequest = (HttpWebRequest)asyncResponse.AsyncState;
+          var someResponse = (Stream)responseRequest.EndGetRequestStream(asyncResponse);
+          taskComplete.TrySetResult(someResponse);
+        } catch (WebException webExc) {
+          var failedResponse = (HttpWebResponse)webExc.Response;
+          taskComplete.TrySetResult(null);
+        }
+      }, request);
+      return taskComplete.Task;
+    }
+  } 
+}
---
+++ Buch.Beispiele/Kapitel 02/FeedReader/FeedReader/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FeedReader.Resources;
+using System.Threading.Tasks;
+using FeedReader.Model;
+using System.Xml.Linq;
+
+using FeedReader.Code.Utilities;
+using System.IO;
+using System.Globalization;
+
+
+namespace FeedReader {
+  public partial class MainPage : PhoneApplicationPage {
+    // Konstruktor
+    public MainPage() {
+      InitializeComponent();
+    }
+
+
+    private async Task<IEnumerable<FeedItem>> UpdateFeed(string url) {
+
+      string result = "";
+      var request = WebRequest.CreateHttp(url);
+      request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+      var response = await request.GetResponseAsync();
+
+      using (var stream = response.GetResponseStream()) {
+        using (var reader = new StreamReader(stream)) {
+          result = await reader.ReadToEndAsync();
+        }
+      }
+
+      var feed = XElement.Parse(result);
+
+      var articles = from item in feed.Descendants("item")
+                     select new FeedItem() {
+                       Title = item.Element("title").Value,
+                       DatePublished = item.Element("pubDate").Value,
+                       Description = item.Element("description").Value,
+                       ArticleURL = item.Element("guid").Value
+                     };
+
+      return articles;
+    }
+
+    private async void btnRefresh_Click(object sender, RoutedEventArgs e) {
+      var articles = await UpdateFeed("http://dotnetautor.de/GetRssFeed");
+      lbFeed.ItemsSource = articles;
+
+    }
+
+  }
+}
